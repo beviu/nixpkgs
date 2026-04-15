0b8a8f71f2ab86e9db5242665d67ff1650592823
beviu <contact@beviu.com>
2026-04-15 17:46:46 +0200

    nixos/bcachefs: Add upstream Systemd units to initrd
    The bcachefs-wait@.service unit can be added as a Wants dependency to a
    .mount units to wait until every device in the filesystem is initialized
    before doing the mount.

--- nixos/modules/tasks/filesystems/bcachefs.nix
+++ nixos/modules/tasks/filesystems/bcachefs.nix
@@ -285,6 +285,7 @@ in
           commonFunctions + lib.concatStrings (lib.mapAttrsToList openCommand bootFs)
         );
 
+        boot.initrd.systemd.packages = [ cfg.package ];
         boot.initrd.systemd.services = lib.mapAttrs' (mkUnits "/sysroot") bootFs;
       })
 
